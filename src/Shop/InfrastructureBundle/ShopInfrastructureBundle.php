f59dc15813a4285f463f02e8eddc10048984e387
Igor Gaponov <jiminy96@gmail.com>
2015-12-14 10:12:00 +0700

    Added Infrastructure bundle

---
+++ src/Shop/InfrastructureBundle/ShopInfrastructureBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shop\InfrastructureBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopInfrastructureBundle extends Bundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
