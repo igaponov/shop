4b7d185a13a0c9c6fa8f9609df06b4fc3e429dd9
Igor Gaponov <jiminy96@gmail.com>
2015-12-14 16:24:58 +0700

    fix index.php for symfony 3.0

--- web/app.php
+++ web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../var/bootstrap.php.cache';
 
 require_once __DIR__.'/../app/AppKernel.php';
 
--- web/app_dev.php
+++ web/app_dev.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 $loader = require __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../var/bootstrap.php.cache';
 Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
